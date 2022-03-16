acdd2fe4b3d5aec51dcfc7cca62f933db75d6474
Jhonata Braga <jhonatavbrg@gmail.com>
2022-03-16 10:10:14 -0300

    add req 02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    stderr = sys.stderr
+
+    if path_file.endswith("txt"):
+        try:
+            with open(path_file, "r") as file:
+                return file.read().splitlines()
+        except FileNotFoundError:
+            return print(f"Arquivo {path_file} não encontrado", file=stderr)
+
+    print('Formato inválido', file=stderr)
